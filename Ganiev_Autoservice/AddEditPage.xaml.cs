2e56e37757cd2ac488e6082220c411426961d89d
NothMea <ganievt2006@mail.ru>
2024-10-16 17:10:41 +0500

    Добавьте файлы проекта.

---
+++ Ganiev_Autoservice/AddEditPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ganiev_Autoservice
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Service _currentService = new Service();
+
+        public AddEditPage(Service SelectedService)
+        {
+            InitializeComponent();
+
+            if (SelectedService != null)
+            {
+                _currentService = SelectedService;
+            }
+
+            DataContext = _currentService;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_currentService.Title))
+                errors.AppendLine("укажите название услуги");
+
+            if (_currentService.Cost <= 0)
+                errors.AppendLine("Правильно укажите стоимость услуги");
+
+            if (_currentService.Discount < 0 || _currentService.Discount == null)
+                errors.AppendLine("Укажите скидку правильно");
+
+            if (string.IsNullOrWhiteSpace(_currentService.DurationInSeconds))
+                errors.AppendLine("Укажите длительность услуги");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (_currentService.ID == 0)
+                Ganiev_autoserviceEntities.GetContext().Service.Add(_currentService);
+
+            try
+            {
+                Ganiev_autoserviceEntities.GetContext().SaveChanges();
+                MessageBox.Show("информация сохранена");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
